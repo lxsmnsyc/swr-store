8eea8720ecc4c643622747bbefffa8ebef95d7c3
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-04-05 15:18:27 +0800

    Update exports

--- packages/react-swr-store/src/index.ts
+++ packages/react-swr-store/src/index.ts
@@ -1,5 +1,7 @@
 import useSWRStore from './use-swr-store';
 export * from './use-swr-store';
+
+export { default as useSWRStore } from './use-swr-store';
 export { default as SWRStoreRoot } from './SWRStoreRoot';
 
 /** @deprecated use `import { useSWRStore } from 'react-swr-store'` instead */
