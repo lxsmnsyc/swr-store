422659063e2befd72e6b1cf5231da5a72900abb7
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-04-05 15:05:14 +0800

    Change export structure

--- packages/react-swr-store/src/index.ts
+++ packages/react-swr-store/src/index.ts
@@ -1,3 +1,6 @@
 import useSWRStore from './use-swr-store';
+export * from './use-swr-store';
+export { default as SWRStoreRoot } from './SWRStoreRoot';
 
+/** @deprecated use `import { useSWRStore } from 'react-swr-store'` instead */
 export default useSWRStore;
