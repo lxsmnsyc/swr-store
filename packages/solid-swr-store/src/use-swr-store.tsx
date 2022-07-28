b28fc3328252fbf1eb3fb798ea2108cdc75c5ee3
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-07-28 20:42:29 +0800

    Bump dependencies; fix `solid-swr-store`

--- packages/solid-swr-store/src/use-swr-store.tsx
+++ packages/solid-swr-store/src/use-swr-store.tsx
@@ -12,52 +12,6 @@ interface UseSWRStoreOptions<T> {
   shouldRevalidate?: boolean;
 }
 
-export function useSWRStore<T, P extends any[] = []>(
-  store: SWRStore<T, P>,
-  args: () => P,
-  options: UseSWRStoreOptions<T>,
-): Resource<T | undefined> {
-  const [resource, { mutate }] = createResource(
-    () => ({
-      args: args(),
-      options: {
-        shouldRevalidate: options.shouldRevalidate,
-      },
-    }),
-    async (params): Promise<T> => {
-      const result = store.get(params.args, {
-        shouldRevalidate: params.options.shouldRevalidate,
-      });
-      if (result.status === 'failure') {
-        throw result.data;
-      }
-      return result.data;
-    },
-    {
-      initialValue: options.initialData,
-    },
-  );
-
-  createEffect(() => {
-    const currentArgs = args();
-    onCleanup(store.subscribe(currentArgs, () => {
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      mutate(() => {
-        const result = store.get(currentArgs, {
-          shouldRevalidate: options.shouldRevalidate,
-          initialData: options.initialData,
-        });
-        if (result.status === 'failure') {
-          throw result.data;
-        }
-        return result.data;
-      });
-    }));
-  });
-
-  return resource as Resource<T | undefined>;
-}
-
 export function useSWRStoreSuspenseless<T, P extends any[] = []>(
   store: SWRStore<T, P>,
   args: () => P,
@@ -73,10 +27,30 @@ export function useSWRStoreSuspenseless<T, P extends any[] = []>(
     onCleanup(store.subscribe(currentArgs, () => {
       setResult(() => store.get(currentArgs, {
         shouldRevalidate: options.shouldRevalidate,
-        initialData: options.initialData,
       }));
     }));
   });
 
   return result;
+}
+
+export function useSWRStore<T, P extends any[] = []>(
+  store: SWRStore<T, P>,
+  args: () => P,
+  options: UseSWRStoreOptions<T>,
+): Resource<T | undefined> {
+  const suspenseless = useSWRStoreSuspenseless(store, args, options);
+  const [resource] = createResource(
+    suspenseless,
+    async (result): Promise<T> => {
+      if (result.status === 'failure') {
+        throw result.data;
+      }
+      return result.data;
+    },
+    {
+      initialValue: options.initialData,
+    },
+  );
+  return resource as Resource<T | undefined>;
 }
